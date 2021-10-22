7c9f2a9c4b15ec0eb54c130445fee50773df319e
Gustavo Piucco <gustavopiucco@gmail.com>
2021-10-21 23:48:23 -0300

    clean

--- src/services/multilevel.service.js
+++ src/services/multilevel.service.js
@@ -16,26 +16,6 @@ async function getByLevel(loggedInUser, level) {
     return multilevel;
 }
 
-// async function getSponsorsIdByUserId(rootUserId) {
-//     let sponsors = [];
-
-//     const rootUser = await userModel.getById(rootUserId);
-
-//     let currentUserId = rootUser.sponsor_id;
-
-//     for (let i = 1; i <= maxLevels; i++) {
-//         const user = await userModel.getById(currentUserId);
-
-//         if (!user) break;
-
-//         sponsors.push(user.id);
-
-//         currentUserId = user.sponsor_id;
-//     }
-
-//     return sponsors;
-// }
-
 async function getSponsorsByUserId(rootUserId) {
     let sponsors = [];
 
@@ -75,23 +55,6 @@ async function payMultilevelBonus(rootUserId, planId) {
         await multilevelRecordsModel.create(user.id, rootUserId, 'affiliate_program', level, value, new Date());
     }
 }
-
-// async function payMultilevelBonus(rootUserId, planId) {
-//     const plan = await planModel.getById(planId);
-
-//     const sponsorsId = await getSponsorsIdByUserId(rootUserId);
-
-//     for (let level = 1; level <= sponsorsId.length; level++) {
-//         const userId = sponsorsId[level - 1];
-//         const value = ((bonusPercentageByLevel[level - 1]) / 100) * plan.price;
-
-//         await userModel.addPendingBalance(userId, value);
-
-//         await multilevelRecordsModel.create(userId, rootUserId, 'affiliate_program', level, value, new Date());
-
-//         await cycleService.handleContractCycle(userId, value);
-//     }
-// }
 
 module.exports = {
     getByLevel,
