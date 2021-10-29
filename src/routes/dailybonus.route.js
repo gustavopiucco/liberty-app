65ab19dcc9cf4eae59d84ec69df375955687d50c
Gustavo Piucco <gustavopiucco@gmail.com>
2021-10-28 23:20:31 -0300

    fix

--- src/routes/dailybonus.route.js
+++ src/routes/dailybonus.route.js
@@ -6,14 +6,45 @@ const auth = require('../middlewares/auth');
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const validate = require('../middlewares/validate');
+const { format, subDays } = require('date-fns');
 const dailyBonusValidation = require('../validations/dailybonus.validation');
 const dailyBonusModel = require('../models/dailybonus.model');
 const planModel = require('../models/plan.model');
 
-// router.get('/daily-bonus', auth('get_daily_bonus'), dailyBonusController.getAll);
-// router.get('/daily-bonus/all/:days', auth('get_all_daily_bonus_days_ago'), validate(dailyBonusValidation.getAllDaysAgo), dailyBonusController.getAllDaysAgo);
+router.get('/', auth('get_daily_bonus'), catchAsync(async (req, res) => {
+    const dailyBonuses = await dailyBonusModel.getAll();
 
-router.get('/daily-bonus/:date', auth('get_daily_bonus'), validate(dailyBonusValidation.getByDate), catchAsync(async (req, res) => {
+    res.status(httpStatus.OK).send(dailyBonuses);
+}));
+
+router.get('/all/:days', auth('get_all_daily_bonus_days_ago'), validate(dailyBonusValidation.getAllDaysAgo), catchAsync(async (req, res) => {
+    let data = [];
+    const todayDate = new Date();
+    const dailyBonuses = await dailyBonusModel.getAll();
+
+    for (let i = 0; i < req.params.days; i++) {
+        let date = subDays(todayDate, i);
+        const dateFormatted = format(date, 'yyyy-MM-dd');
+
+        let percentage = 0;
+
+        for (let i = 0; i < dailyBonuses.length; i++) {
+            if (dailyBonuses[i].date == dateFormatted) {
+                percentage = dailyBonuses[i].percentage;
+                break;
+            }
+        }
+
+        data.push({
+            date: dateFormatted,
+            percentage: percentage
+        });
+    }
+
+    res.status(httpStatus.OK).send(data);
+}));
+
+router.get('/:date', auth('get_daily_bonus'), validate(dailyBonusValidation.getByDate), catchAsync(async (req, res) => {
     const dailyBonus = await dailyBonusModel.getByDate(req.params.date);
 
     if (!dailyBonus) {
